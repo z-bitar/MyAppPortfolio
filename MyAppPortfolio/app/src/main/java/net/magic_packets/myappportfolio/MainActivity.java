120938bb1acbb4c7ca8b6bc49b37234a0df40403
unknown <z.bitar@outlook.com>
2015-09-26 13:47:19 +0300

    Code Review Suggestions Applied

--- MyAppPortfolio/app/src/main/java/net/magic_packets/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/net/magic_packets/myappportfolio/MainActivity.java
@@ -2,8 +2,6 @@ package net.magic_packets.myappportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,28 +30,7 @@ public class MainActivity extends AppCompatActivity implements
         btnbacon.setOnClickListener(this);
         btncapstone.setOnClickListener(this);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    
 
     @Override
     public void onClick(View view) {
